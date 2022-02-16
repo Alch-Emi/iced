d5d5be7897d0867fe8627181535f467dc055cac4
Emi Simpson <emi@alchemi.dev>
2022-02-16 12:23:57 -0500

    Flesh out the dialog options

--- native/src/dialog.rs
+++ native/src/dialog.rs
@@ -13,6 +13,8 @@
 //! [`Command`]: iced_native::Command
 
 use iced_futures::MaybeSend;
+use std::borrow::Borrow;
+use std::borrow::Cow;
 use std::fmt;
 use std::path::PathBuf;
 
@@ -22,7 +24,12 @@ pub enum Action<Msg> {
     ///
     /// These dialogs normally take the form of a small window with a message
     /// and one or two buttons.
-    MessageDialog(MessageDialogOptions, MessageDialogVariant<Msg>),
+    MessageDialog(
+        MessageDialogOptions,
+        MessageDialogVariant<Msg>,
+        /// The message that the message dialog should display
+        Option<Cow<'static, str>>,
+    ),
 
     /// Produce a file dialog
     ///
@@ -36,7 +43,63 @@ pub enum Action<Msg> {
 
 /// Various options common to all message dialogs
 #[derive(Debug)]
-pub struct MessageDialogOptions {}
+pub struct MessageDialogOptions {
+    /// The severity to associate with this dialog
+    pub level: MessageLevel,
+
+    /// The dialog window's title
+    pub title: Option<Cow<'static, str>>,
+}
+
+impl MessageDialogOptions {
+    /// Create a new message dialog with a severity of INFO
+    pub const fn new() -> Self {
+        Self {
+            level: MessageLevel::Info,
+            title: None,
+        }
+    }
+
+    /// Create a new message dialog with a severity of INFO and the given title
+    pub const fn new_with_title(title: &'static str) -> Self {
+        Self {
+            level: MessageLevel::Info,
+            title: Some(Cow::Borrowed(title)),
+        }
+    }
+
+    /// Set the title of the message dialog
+    pub fn with_title(self, title: impl ToString) -> Self {
+        Self {
+            title: Some(Cow::Owned(title.to_string())),
+            ..self
+        }
+    }
+
+    /// Set the severity of the message dialog
+    pub fn with_level(self, level: MessageLevel) -> Self {
+        Self { level, ..self }
+    }
+}
+
+/// Possible severity levels for message dialogs
+#[derive(Debug)]
+pub enum MessageLevel {
+    /// Give the user some information
+    ///
+    /// This typically doesn't require any urgent action, and does not bode
+    /// unwell.
+    Info,
+
+    /// Alert the user of a non-fatal problem
+    ///
+    /// Execution can continue, but may imply a risk, or could lead to an error
+    /// or unexpected behaviour down the line
+    Warning,
+
+    /// There was a serious problem, and the program couldn't complete its goal
+    Error,
+}
 
 /// Different variants of message dialogs
 ///
@@ -72,7 +135,174 @@ pub enum MessageDialogVariant<Msg> {
 
 /// Assorted options and filters that can be applied to any kind of file dialog
 #[derive(Debug)]
-pub struct FileDialogOptions {}
+pub struct FileDialogOptions {
+    /// A list of filters that should be available to the user
+    ///
+    /// Defaults to no filters (e.g. show all files)
+    pub filters: Cow<'static, [Filter]>,
+
+    /// Sets the directory that the prompt will start on
+    pub initial_directory: Option<PathBuf>,
+
+    /// Sets the initial value of the filename
+    ///
+    /// Particularly useful for save dialogs
+    pub initial_filename: Option<Cow<'static, str>>,
+
+    /// Sets the dialog window title
+    pub title: Option<Cow<'static, str>>,
+}
+
+impl FileDialogOptions {
+    /// Create a new [`FileDialogOptions`] with the default values
+    pub const fn new() -> Self {
+        Self::new_with_filters(&[])
+    }
+
+    /// Create a new [`FileDialogOptions`] with a set of filters
+    ///
+    /// All other values are set to defaults
+    pub const fn new_with_filters(filters: &'static [Filter]) -> Self {
+        FileDialogOptions {
+            filters: Cow::Borrowed(filters),
+            initial_directory: None,
+            initial_filename: None,
+            title: None,
+        }
+    }
+
+    /// Add a new filter to the existing filters
+    pub fn add_filter(mut self, filter: Filter) -> Self {
+        self.filters.to_mut().push(filter);
+        self
+    }
+
+    /// Set the initial directory of the dialog
+    pub fn with_initial_directory(self, path: PathBuf) -> Self {
+        Self {
+            initial_directory: Some(path),
+            ..self
+        }
+    }
+
+    /// Set the initial filename of the dialog
+    pub fn with_initial_filename(self, filename: impl ToString) -> Self {
+        Self {
+            initial_filename: Some(Cow::Owned(filename.to_string())),
+            ..self
+        }
+    }
+
+    /// Set the dialog's title
+    pub fn with_title(self, title: impl ToString) -> Self {
+        Self {
+            title: Some(Cow::Owned(title.to_string())),
+            ..self
+        }
+    }
+}
+
+/// A filter for the kinds of files that can be selected from a file dialog
+///
+/// This often takes the form of a category of file (say, "Music Files") along
+/// with a series of file extensions that fall into that category ("flac",
+/// "opus", "mp3")
+///
+/// The user will be able to use these filters to make finding the file they are
+/// looking for easier.  However, these filters don't restrict the user, so
+/// there's no guarantee that the selected file will comply with one of the
+/// filters.
+#[derive(Debug, Clone)]
+pub enum Filter {
+    /// The normal form of a filter, with owned values for names and extensions
+    Owned {
+        /// The name of the filter (e.g. "Music Files")
+        name: String,
+        /// The list of valid extensions (e.g. "mp3", ...)
+        extensions: Vec<String>,
+    },
+
+    /// A compile-time constant filter
+    ///
+    /// This is practically identical to the [`Self::Owned`] variant, but can
+    /// (and must) be a compile time constant
+    Static {
+        /// The name of the filter (e.g. "Music Files")
+        name: &'static str,
+        /// The list of valid extensions (e.g. "mp3", ...)
+        extensions: &'static [&'static str],
+    },
+}
+
+impl Filter {
+    /// Construct a new [`Filter`] using `&'static str`s
+    ///
+    /// ## Example
+    /// ```
+    /// # use iced_native::dialog::Filter;
+    /// let name = "Music Files";
+    /// let extensions = &["mp3", "flac", "ogg"];
+    /// let filter = Filter::new_const(name, extensions);
+    /// # assert_eq!(filter.name(), "Music Files");
+    /// # assert_eq!(
+    /// #   filter.extensions(),
+    /// #   vec![
+    /// #       "mp3".to_owned(),
+    /// #       "flac".to_owned(),
+    /// #       "ogg".to_owned()
+    /// #   ]
+    /// # );
+    /// ```
+    pub const fn new_const(
+        name: &'static str,
+        extensions: &'static [&'static str],
+    ) -> Self {
+        Self::Static { name, extensions }
+    }
+
+    /// Construct a new [`Filter`]
+    ///
+    /// ## Example
+    /// ```
+    /// # use iced_native::dialog::Filter;
+    /// let name = "Music Files".to_string();
+    /// let extensions = vec!["mp3".to_string(), "flac".to_string(), "ogg".to_string()];
+    /// let filter = Filter::new(name, extensions);
+    /// # assert_eq!(filter.name(), "Music Files");
+    /// # assert_eq!(
+    /// #   filter.extensions(),
+    /// #   vec![
+    /// #       "mp3".to_owned(),
+    /// #       "flac".to_owned(),
+    /// #       "ogg".to_owned()
+    /// #   ]
+    /// # );
+    /// ```
+    pub fn new(name: impl Into<String>, extensions: Vec<String>) -> Self {
+        Self::Owned {
+            name: name.into(),
+            extensions,
+        }
+    }
+
+    /// Get the filter name
+    pub fn name(&self) -> &str {
+        match self {
+            Self::Static { name, .. } => name,
+            Self::Owned { name, .. } => name.as_str(),
+        }
+    }
+
+    /// Get a list of extensions in this filter
+    pub fn extensions(&self) -> Vec<String> {
+        match self {
+            Self::Static { extensions, .. } => {
+                extensions.iter().map(|s| (*s).to_owned()).collect()
+            }
+            Self::Owned { extensions, .. } => extensions.clone(),
+        }
+    }
+}
 
 /// Different variants on file dialogs
 ///
@@ -139,8 +369,8 @@ impl<Msg> Action<Msg> {
         Mapper: FnOnce(Msg) -> MappedMsg + 'static + MaybeSend + Sync,
     {
         match self {
-            Self::MessageDialog(options, variant) => {
-                Action::MessageDialog(options, variant.map(f))
+            Self::MessageDialog(options, variant, text) => {
+                Action::MessageDialog(options, variant.map(f), text)
             }
             Self::FileDialog(options, variant) => {
                 Action::FileDialog(options, variant.map(f))
@@ -209,8 +439,14 @@ impl<Msg> FileDialogVariant<Msg> {
 impl<T> fmt::Debug for Action<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Action::MessageDialog(options, variant) => {
-                write!(f, "MessageDialog({:?}, {:?})", options, variant)
+            Action::MessageDialog(options, variant, text) => {
+                write!(
+                    f,
+                    "MessageDialog({:?}, {:?} {:?})",
+                    options,
+                    variant,
+                    text.borrow()
+                )
             }
             Action::FileDialog(options, variant) => {
                 write!(f, "FileDialog({:?}, {:?})", options, variant)
